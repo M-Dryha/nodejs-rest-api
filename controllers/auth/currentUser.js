896659afd258d11591ba13051279cdfbedec5983
Mariia Dryha <m.pishchanska@gmail.com>
2022-09-09 23:16:46 +0300

    feat: add updateAvatar

--- controllers/auth/currentUser.js
+++ controllers/auth/currentUser.js
@@ -1,12 +1,8 @@
-const { User } = require("../../models");
-
 const currentUser = async (req, res) => {
-  const { _id } = req.user;
-  const curUser = await User.findById({ _id });
   res.json({
     data: {
-      email: curUser.email,
-      subscription: curUser.subscription,
+      email: req.user.email,
+      subscription: req.user.subscription,
     },
   });
 };
